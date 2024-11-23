0d7765d015fffa554b979b684a878dd779d0892b
Gwyn <skythekid211@gmail.com>
2024-11-23 19:03:41 +0800

    mongo debi firebase config

--- frontend/src/pages/firebaseConfig.js
+++ frontend/src/pages/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnbVXV3mtDFO5j7CNys17l9OTlxrFZUbE",
-  authDomain: "mern-project-86af8.firebaseapp.com",
-  projectId: "mern-project-86af8",
-  storageBucket: "mern-project-86af8.firebasestorage.app",
-  messagingSenderId: "351527233765",
-  appId: "1:351527233765:web:43dff29e9103b777124c0c",
-  measurementId: "G-SCQEQER7LJ"
+  apiKey: "AIzaSyAuAHNRTGWyL-y8QGwX43YGED5j8n2HIf0",
+  authDomain: "mern-stack-2ff5b.firebaseapp.com",
+  projectId: "mern-stack-2ff5b",
+  storageBucket: "mern-stack-2ff5b.firebasestorage.app",
+  messagingSenderId: "41775657194",
+  appId: "1:41775657194:web:fd1ce6d3128b492e846793",
+  measurementId: "G-DHVKMD47ZE"
 };
 
 const app = initializeApp(firebaseConfig);
